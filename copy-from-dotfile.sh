8a868114c6f1911013f4716a64eb72329bb01ada
Kyeong Soo <penghunix@gmail.com>
2024-09-03 18:26:52 +0900

    fix: copy scripts changed to rsync

--- copy-from-dotfile.sh
+++ copy-from-dotfile.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
 
 
-cp -rf ~/.config/emacs/elisp/* elisp
-cp -f ~/.config/emacs/init.el init.el
+SRC_DIR="$HOME/.config/emacs"
+DEST_DIR="$1"
+
+if ! [[ -d "$DEST_DIR" ]]; then
+    echo "Usage copy-from-dotfile.sh DEST_DIR"
+    exit 1
+fi
+
+# -r: recursive
+# -v: verbose
+# -z: compress
+# -t: --times, preserve modification times
+# -U: --atiems, preserve access (use) times
+rsync -rvzt -U --progress "$SRC_DIR/elisp/" "$DEST_DIR/elisp/"
+cp -p "$SRC_DIR/init.el" "$DEST_DIR/init.el"
--- copy-to-dotfile.sh
+++ copy-to-dotfile.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 
-cp -rf elisp ~/.config/emacs/elisp
-cp -f init.el ~/.config/emacs/init.el
+
+SRC_DIR="$1"
+DEST_DIR="$HOME/.config/emacs"
+
+if ! [[ -d "$SRC_DIR" ]]; then
+    echo "Usage copy-to-dotfile.sh SRC_DIR"
+    exit 1
+fi
+
+# -r: recursive
+# -v: verbose
+# -z: compress
+# -t: --times, preserve modification times
+# -U: --atiems, preserve access (use) times
+rsync -rvzt -U --progress "$SRC_DIR/elisp/" "$DEST_DIR/elisp/"
+cp -p "$SRC_DIR/init.el" "$DEST_DIR/init.el"
+
