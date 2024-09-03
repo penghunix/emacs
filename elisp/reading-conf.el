965dec391d415ac290cb6899eaa8143a30b91f0a
Kyeongsoo Choi <penghunix@gmail.com>
2024-09-04 02:44:19 +0900

    fix: fix nov.el config

--- elisp/reading-conf.el
+++ elisp/reading-conf.el
@@ -2,6 +2,35 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'justify-kp)
+
+(defun my-nov-window-configuration-change-hook ()
+  (my-nov-post-html-render-hook)
+  (remove-hook 'window-configuration-change-hook
+               'my-nov-window-configuration-change-hook
+               t))
+
+(defun my-nov-post-html-render-hook ()
+  (if (get-buffer-window)
+      (let ((max-width (pj-line-width))
+            buffer-read-only)
+        (save-excursion
+          (goto-char (point-min))
+          (while (not (eobp))
+            (when (not (looking-at "^[[:space:]]*$"))
+              (goto-char (line-end-position))
+              (when (> (shr-pixel-column) max-width)
+                (goto-char (line-beginning-position))
+                (pj-justify)))
+            (forward-line 1))))
+    (add-hook 'window-configuration-change-hook
+              'my-nov-window-configuration-change-hook
+              nil t)))
+
+(defun nov-font-setup ()
+  (face-remap-add-relative 'variable-pitch :family "JetbrainsMono Nerd Font"
+                                           :height 1.0))
+
 (use-package nov
   :ensure t
   :config
@@ -9,6 +38,8 @@
   (setq visual-fill-column-center-text t)
   (add-hook 'nov-mode-hook 'visual-line-mode)
   (add-hook 'nov-mode-hook 'visual-fill-column-mode)
+  (add-hook 'nov-mode-hook 'nov-font-setup)
+  ;; (add-hook 'nov-post-html-render-hook 'my-nov-post-html-render-hook)
   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
 
 (provide 'reading-conf)
